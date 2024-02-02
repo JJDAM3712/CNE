d6b326b7cc01f41221781567201d0909df952270
BRIAN RANGEL <brian.brigor@gmail.com>
2024-02-02 01:16:46 -0400

    Agrega La primera Prueba de Rutas y el Menu de sel

--- cne_project/src/App.js
+++ cne_project/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import  Layout from "./pages/Layout";
+import  About from "./pages/About";
+import  Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <p>Prueba de Fuego</p> 
+    <div>
+      <h1>Rutas</h1>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='about' element={<About />} />
+          <Route path='/' element={<Home />} />
+          <Route path='*' element={<Home />} />
+
+        </Route>
+      </Routes>
     </div>
+    
   );
 }
 
