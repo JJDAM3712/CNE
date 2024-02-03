b5d7238321268d579f1f9ece6c9d154316dafd1d
BRIAN RANGEL <brian.brigor@gmail.com>
2024-02-03 04:00:58 -0400

    Estructura y diseño Base del proyecto, Sidebar agg

--- cne_project/src/App.js
+++ cne_project/src/App.js
@@ -1,7 +1,6 @@
-import './App.css';
-import './input.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
+import '../src/css/App.css';
+import './output.css';
+import { Routes, Route } from "react-router-dom";
 import Asistencias from "./pages/Asistencias";
 import Visitas from "./pages/Visitas";
 import Departamentos from "./pages/Departamentos";
@@ -9,24 +8,29 @@ import Usuario from "./pages/Usuario";
 import Personal from "./pages/Personal";
 import Cargos from "./pages/Cargos";
 import Home from "./pages/Home";
+import Side from "./components/Sidebar2";
 
 function App() {
   return (
-    <aside>
-      <h1>Menu</h1>
-        <Routes>
-          <Route path='/' element={<Sidebar />}>
-            <Route path="personal" element={<Personal />} />
-            <Route path="asistencias" element={<Asistencias />} />
-            <Route path="visitas" element={<Visitas />} />
-            <Route path="departamentos" element={<Departamentos />} />
-            <Route path="cargos" element={<Cargos />} />
-            <Route path="usuario" element={<Usuario />} />
-            <Route path="*" element={<Home />} />
-          </Route>
-        </Routes>
-    </aside>
+    <div className='App'>
+      <div className='header'>
 
+      </div>
+      <div className='sidebar'>
+        <Side />
+      </div>
+      <div className='body'>
+        <Routes>
+          <Route path="personal" element={<Personal />} />
+          <Route path="asistencias" element={<Asistencias />} />
+          <Route path="visitas" element={<Visitas />} />
+          <Route path="departamentos" element={<Departamentos />} />
+          <Route path="cargos" element={<Cargos />} />
+          <Route path="usuario" element={<Usuario />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
---
+++ cne_project/src/components/Sidebar2.jsx
@@ -0,0 +1,43 @@
+
+import { Sidebar } from 'flowbite-react';
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from 'react-icons/hi';
+
+function Side() {
+	return (
+		<Sidebar aria-label="Default sidebar example">
+			<Sidebar.Items>
+				<Sidebar.ItemGroup>
+					<Sidebar.Item icon={HiChartPie}> <Link to="/">Pagina Principal</Link>
+					</Sidebar.Item>
+					<Sidebar.Item icon={HiInbox}>
+						<Link to="/personal">Personal</Link>
+					</Sidebar.Item>
+					<Sidebar.Item icon={HiUser} label="3">
+						<Link to="/asistencias">Asistencias</Link>
+					</Sidebar.Item>
+					<Sidebar.Item icon={HiShoppingBag}>
+						<Link to="/visitas">Visitas</Link>
+					</Sidebar.Item>
+					<Sidebar.Item icon={HiArrowSmRight}>
+						<Link to="/departamentos">Departamento</Link>
+					</Sidebar.Item>
+					<Sidebar.Item icon={HiTable}>
+						<Link to="/cargos">Cargo</Link>
+					</Sidebar.Item>
+					<Sidebar.Item icon={HiViewBoards}>
+						<Link to="/usuario" >Usuarios</Link>
+					</Sidebar.Item>
+					<Sidebar.Item>
+						<Link to="/salir">Salir</Link>
+					</Sidebar.Item>
+				</Sidebar.ItemGroup>
+			</Sidebar.Items>
+			<hr />
+			<Outlet />
+		</Sidebar>
+
+	);
+}
+export default Side;
