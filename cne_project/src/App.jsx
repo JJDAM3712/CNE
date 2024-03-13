057586f1bfd174543d10c948dbabd9335cb856f4
Brigor17 <brian.brigor@gmail.com>
2024-03-13 08:38:28 -0400

    Cambio a vite

--- cne_project/src/App.jsx
+++ cne_project/src/App.jsx
@@ -5,68 +5,64 @@ import { Sidebar } from "./components/sidebar"; //Sidebar
 import { Migas } from "./components/Breadcrumb"; // Asegúrate de que la ruta de importación sea correcta
 import styled from "styled-components"; //Componente para Estilos directo al JSX
 import React from "react";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import { useState } from "react";
 import { ThemeProvider } from "styled-components";
-import { useLocation } from "react-router-dom"; // Importa useLocation
 export const ThemeContext = React.createContext(null);
 
 function App() {
-  // Funcion que define el Tema de color Actual
   const [theme, setTheme] = useState("light");
   const themeStyle = theme === "light" ? Light : Dark;
-  // *****************************
-  // Constante para saber definir si el Sidebar esta Activo o No
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-  const location = useLocation(); // Usa useLocation para obtener la ruta actual
-
+  const [sidebarOpen, setSidebarOpen] = useState(true);
   return (
     <>
       <ThemeContext.Provider value={{ setTheme, theme }}>
         <ThemeProvider theme={themeStyle}>
-          <Container>
-            <header className="Header">
-              <Migas activePath={location.pathname} />{" "}
-              {/* Pasa la ruta activa como prop a Migas */}
-            </header>
-            {/* CONTENEDOR PRINCIPAL DEL BODY */}
-            <main
-              className={sidebarOpen ? "sidebarState active" : "sidebarState"}
-            >
-              {/* SIDEBAR */}
-              <Sidebar
-                sidebarOpen={sidebarOpen}
-                setSidebarOpen={setSidebarOpen}
-              />
-              {/* SIDEBAR */}
-              <MyRoutes />
-            </main>
-          </Container>
+          <BrowserRouter>
+            <Container>
+              <header className="Header">
+                <Migas activePath={location.pathname} />{" "}
+                {/* Pasa la ruta activa como prop a Migas */}
+              </header>
+              {/* CONTENEDOR PRINCIPAL DEL BODY */}
+              <main
+                className={sidebarOpen ? "sidebarState active" : "sidebarState"}
+              >
+                {/* SIDEBAR */}
+                <Sidebar
+                  sidebarOpen={sidebarOpen}
+                  setSidebarOpen={setSidebarOpen}
+                />
+                {/* SIDEBAR */}
+                <MyRoutes />
+              </main>
+            </Container>
+          </BrowserRouter>
         </ThemeProvider>
       </ThemeContext.Provider>
     </>
   );
 }
-// #region ESTILOS DEL CONTENEDOR GENERAL
 const Container = styled.div`
 
-  .Header {
-    background: ${({ theme }) => theme.bgHeader};
-    height: 3rem;
-    display: flex;
-    align-items: flex-end;
-    flex-direction: row;
-    justify-content: flex-start;
-    color:${({ theme }) => theme.text};
+.Header {
+  background: ${({ theme }) => theme.bgHeader};
+  height: 3rem;
+  display: flex;
+  align-items: flex-end;
+  flex-direction: row;
+  justify-content: flex-start;
+  color:${({ theme }) => theme.text};
+}
+.sidebarState {
+  display: grid;
+  grid-template-columns: 90px auto;
+  background: ${({ theme }) => theme.bgtotal};
+  transition:all 0.3s ;
+  &.active {
+    grid-template-columns: 300px auto;
   }
-  .sidebarState {
-    display: grid;
-    grid-template-columns: 90px auto;
-    background: ${({ theme }) => theme.bgtotal};
-    transition:all 0.3s ;
-    &.active {
-      grid-template-columns: 300px auto;
-    }
-    color:${({ theme }) => theme.text};
+  color:${({ theme }) => theme.text};
 `;
 // #endregion
 export default App;
